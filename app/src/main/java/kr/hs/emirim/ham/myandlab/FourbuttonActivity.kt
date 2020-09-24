350d3870bb7f42735ba990ca35d8877268f3116c
Gihun Ham <progh2@gmail.com>
2020-09-24 20:51:19 +0900

    p104 버튼 실행 예제

---
+++ app/src/main/java/kr/hs/emirim/ham/myandlab/FourbuttonActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.ham.myandlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FourbuttonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fourbutton)
+    }
+}
